4a2b72d2f50deedb4fb3298db5877823ebfd1f08
jewelcse <jewelcse045@gmail.com>
2023-06-06 11:55:55 +0600

    back button added

--- app/src/main/java/com/jewelcse/hello/HelloActivity.java
+++ app/src/main/java/com/jewelcse/hello/HelloActivity.java
@@ -1,13 +1,31 @@
 package com.jewelcse.hello;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HelloActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HelloActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed(); // Calls the default back button behavior
     }
 }
